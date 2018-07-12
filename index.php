3c9d29e0ad05bdda5ac2590a5df3a4397812c8fc
praneshsingha <praneshsingha@rediffmail.com>
2018-07-12 12:14:13 +0530

    all pages are modified and uploaded

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Eeaka Consentancy Service | Home</title>	
+	<?php 
+		include('bootstrap.php');
+	 ?>	
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!-- <nav class=" navbar  fixed-top navbar-expand-md"> -->	
+	 <nav class="navbar navbar-light fixed-top navbar-expand-md nav">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html" style="font-size: 30px;font-family:'Aldrich', sans-serif;">
+            <img src="logo/logo.png" width="150px;" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto" id="nav-lang">
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " href="index.html">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " href="#section-1">SERVICE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " href="#section-2">CAREERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " href="#section-3">CLIENTS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " href="#section-1">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " style="margin-right: 10px;" href="#section-1">CONTACT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link " style="margin-right: 10px;" href="login.html">LOGIN</a>
+                </li>                                
+                <li class="nav-item">
+                    <a class="nav-link scroll-link btn btn-outline-primary" id="nav-btn" href="#section-4">Get Started</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+									<!-- 2nd section div includes image and quots -->
+
+
+</body>
+</html>	
