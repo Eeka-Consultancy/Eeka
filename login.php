3dbd7cd5be95eb6487654bebb7dabc5cad47083a
praneshsingha <praneshsingha@rediffmail.com>
2018-07-12 12:55:37 +0530

    all pages designed

---
+++ login.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="icon" href="logo/logo.png">
+	<link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+<div class="container-fluid">
+    <a class="navbar-brand" href="index.php" style="font-size: 30px;font-family:'Aldrich', sans-serif;">
+        <img src="logo/logo.png" width="150px;" alt="">
+    </a>
+</div>
+
+<div class="container-fluid" style="margin-top: 10px;">
+	<div class="row" style="margin: 10px 5px 0px 5px;">
+		<div class="col-xl-3 col-md-4 col-sm-12 col-xs-12 offset-xl-3 offset-md-3" style="background-color: rgb(255,255,255);color: black;">
+			<p class=,"text-center" style="font-size: 30px; margin-top: 10px;">LOGIN</p>
+			<form action="" class="form-group">
+				<label for="username">Username</label>
+				<input type="email" class="form-control" required>
+				<label for="username">Password</label>
+				<input type="password" class="form-control" required>
+				<br>				
+				<input type="checkbox"> Remember Me
+				<br>
+				<br>
+				<button class="btn btn-outline-dark" style="padding: 5px 42px 5px 42px;">LOGIN</button><br> <br>
+			</form>
+				<a href="register.php">
+					<button class="btn btn-outline-dark" style="padding: 5px 30px 5px 30px;margin-bottom: 20px;">GO TO REGISTER</button>
+				</a>
+		</div>
+		<div class="col-xl-3 col-md-3 col-sm-12 col-sx-12 d-none d-xl-block d-md-block" style="background-color: rgba(255,255,255);">
+			<img src="images/stone.jpg" style="width: 105%;height:500px;" alt="">
+		</div>
+	</div>
+</div>
+</body>
+</html>
--- register.php
+++ register.php
@@ -13,7 +13,8 @@
 	<link rel="stylesheet" href="css/login.css">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="icon" href="logo/logo.png">
-	<link rel="stylesheet" href="css/style.css">
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/login.css">
     <script>
     $(document).ready(function () 
     {
@@ -36,9 +37,14 @@
   </script>
 </head>
 <body>
-<nav class="navbar navbar-light fixed-top navbar-expand-md nav">
+<div class="container-fluid">
+    <a class="navbar-brand" href="index.php" style="font-size: 30px;font-family:'Aldrich', sans-serif;">
+        <img src="logo/logo.png" width="150px;" alt="">
+    </a>
+</div>
+<!-- <nav class="navbar navbar-light fixed-top navbar-expand-md nav">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html" style="font-size: 30px;font-family:'Aldrich', sans-serif;">
+        <a class="navbar-brand" href="index.php" style="font-size: 30px;font-family:'Aldrich', sans-serif;">
         	<img src="logo/logo.png" width="150px;" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -73,16 +79,15 @@
             </ul>
         </div>
     </div>
-</nav>
-<div class="container-fluid" style="margin-top: 80px;">
+</nav> -->
+<div class="container-fluid" style="margin-top: 10px;">
     <div class="row">
-        <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12 offset-xl-3 offset-md-3" style="background-color: rgba(0,0,0,0.8);">
-            <p class="text-center" style="font-size: 30px; margin-top: 10px;color: white">REGISTER</p>
+        <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12 offset-xl-3 offset-md-3" style="background-color: white;">
+            <p class="text-center" style="font-size: 30px; margin-top: 10px;color: black">REGISTER</p>
         </div>
     </div>
 	<div class="row">        
-		<div class="col-xl-3 col-md-4 col-sm-12 col-xs-12 offset-xl-3 offset-md-3" style="background-color: rgba(0,0,0,0.8);color: white;">
-			
+		<div class="col-xl-3 col-md-4 col-sm-12 col-xs-12 offset-xl-3 offset-md-3" style="background-color: white;color: black;">
 			<form action="" class="form-group">
 				<label for="username">FIRST NAME</label>
 				<input type="text" class="form-control" required>
@@ -97,19 +102,9 @@
                 &nbsp;&nbsp;
                 <input type="radio" name="gender"> FEMALE
                 &nbsp;&nbsp;
-                <input type="radio" name="gender"> OTHER
-				<br><br>				
-				<input type="checkbox"> REMEMBER ME
-				<br>
-				<br>
-                <button class="btn btn-outline-light" style="padding: 5px 30px 5px 30px;">REGISTER</button><br> <br>
-			</form>
-                <a href="login.html">
-                    <button class="btn btn-outline-light" style="padding: 5px 39px 5px 40px;margin-bottom: 10px;">GO TO LOGIN</button>
-                </a>
+                <input type="radio" name="gender"> OTHER				        
 		</div>
-        <div class="col-xl-3 col-md-4 col-sm-12 col-xs-12" style="background-color: rgba(0,0,0,0.8);color: white;">            
-            <form action="" class="form-group">
+        <div class="col-xl-3 col-md-4 col-sm-12 col-xs-12" style="background-color: white;color: black;">                        
                 <label for="fname">LAST NAME</label>
                 <input type="text" class="form-control" required>
                 <label for="re-password">RE-PASSWORD</label>
@@ -120,9 +115,17 @@
                 <label for="password">DATE OF BIRTH</label>
                 <input type="date" class="form-control" id="quantity" required>
             </form>         
+        </div>  
+	</div>  
+    <div class="row">
+        <div class="col-xl-6 col-md-6 col-sm-12 col-xs-12 offset-md-3" style="background-color: white;color: black;">
+            <input type="checkbox"> REMEMBER ME
+            <br><br>
+            <input type="submit" name="submit" value="REGISTER" class="btn btn-outline-dark" style="margin-bottom: 20px;">
+            <br>
+            <a href="login.php" class="btn btn-outline-dark" style="margin-bottom: 20px;">GO TO LOGIN</a>
         </div>
-        
-	</div>
+    </div>  
 </div>
 </body>
 </html>
